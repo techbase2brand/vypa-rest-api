316f0a6bdbb0b7f1061af411568e80e39ab96b69
techb2bnew <techbase2brand@gmail.com>
2024-12-30 18:24:44 +0530

    product crud page design

--- src/components/shop/add-procuct.tsx
+++ src/components/shop/add-procuct.tsx
@@ -46,10 +46,14 @@ import { formatSlug } from '@/utils/use-slug';
 import StickyFooterPanel from '@/components/ui/sticky-footer-panel';
 import { socialIcon } from '@/settings/site.settings';
 import { ShopDescriptionSuggestion } from '@/components/shop/shop-ai-prompt';
+import { useInActiveShopsQuery, useShopsQuery } from '@/data/shop';
 import PhoneNumberInput from '@/components/ui/phone-input';
 import DatePicker from '@/components/ui/date-picker';
 import { addDays, addMinutes, isSameDay, isToday } from 'date-fns';
-
+import Multiselect from 'multiselect-react-dropdown';
+import VariantTable from './variant-table';
+import { SortOrder } from '@/types';
+ 
 // const socialIcon = [
 //   {
 //     value: 'FacebookIcon',
@@ -97,6 +101,24 @@ type FormValues = {
   tag?: string;
 };
 const AddProduct = ({ initialValues }: { initialValues?: Shop }) => {
+
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const [page, setPage] = useState(1);
+    const [orderBy, setOrder] = useState('created_at');
+    const [sortedBy, setColumn] = useState<SortOrder>(SortOrder.Desc);
+    const { shops, paginatorInfo, loading, error } = useShopsQuery({
+      name: searchTerm,
+      limit: 10,
+      page,
+      orderBy,
+      sortedBy,
+      is_active: false,
+    });
+
+  const [activeTab, setActiveTab] = useState(1); 
+  const [isChecked, setIsChecked] = useState(false); // State for checkbox
+
   const [location] = useAtom(locationAtom);
   const { mutate: createShop, isLoading: creating } = useCreateShopMutation();
   const { mutate: updateShop, isLoading: updating } = useUpdateShopMutation();
@@ -224,26 +246,87 @@ const AddProduct = ({ initialValues }: { initialValues?: Shop }) => {
       });
     }
   }
+  function handlePagination(current: any) {
+    setPage(current);
+  }
+ 
+
+  // Handle checkbox toggle
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+    if (!isChecked) {
+      // When checkbox is checked, activate Combination tab
+      setActiveTab(2);
+    } else {
+      // When checkbox is unchecked, activate Basic Info tab
+      setActiveTab(1);
+    }
+  };
 
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)} noValidate>
-        <div className="flex justify-between items-center">
-          <b className='text-green bold' style={{color:'#21BA21',fontSize:'20px'}}>Basic Info</b>
-          <div className='flex items-center gap-3' style={{lineHeight:'0px'}}>Does this product have variants?  
-          <div className="relative inline-block w-11 h-5">
-          <input   id="switch-component-greens" type="checkbox" className="peer appearance-none w-11 h-5 bg-red-500 rounded-full checked:bg-green-600 cursor-pointer transition-colors duration-300" />
+        <div className="  justify-between items-center">  
+          <div className="border-b border-gray-200 dark:border-neutral-700 flex justify-between items-center">
+        <nav className="flex gap-4" aria-label="Tabs" role="tablist" aria-orientation="horizontal">
+          <button
+            type="button"
+            className={`hs-tab-active:font-bold hs-tab-active:border-green-600 hs-tab-active:text-green-600 py-4 px-1 inline-flex items-center gap-x-2 border-b-2 border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-green-600 focus:outline-none focus:text-green-600 disabled:opacity-50 disabled:pointer-events-none dark:text-neutral-400 dark:hover:text-green-500 ${activeTab === 1 ? 'font-semibold-600 border-green-600 text-green-600' : ''}`}
+            onClick={() => setActiveTab(1)}
+            id="tabs-with-underline-item-1"
+            aria-selected={activeTab === 1 ? 'true' : 'false'}
+            aria-controls="tabs-with-underline-1"
+            role="tab"
+            style={{color:'#000',fontSize:'18px'}}
+          >
+            Basic Info
+          </button>
+          {isChecked && (
+          <button
+            type="button"
+            className={`hs-tab-active:font-bold hs-tab-active:border-green-600 hs-tab-active:text-green-600 py-4 px-1 inline-flex items-center gap-x-2 border-b-2 border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-green-600 focus:outline-none focus:text-green-600 disabled:opacity-50 disabled:pointer-events-none dark:text-neutral-400 dark:hover:text-green-500 ${activeTab === 2 ? 'font-semibold-600 border-green-600 text-green-600' : ''}`}
+            onClick={() => setActiveTab(2)}
+            id="tabs-with-underline-item-2"
+            aria-selected={activeTab === 2 ? 'true' : 'false'}
+            aria-controls="tabs-with-underline-2"
+            role="tab"
+            style={{color:'#000',fontSize:'18px'}}
+          >
+            Combination
+          </button> 
+           )}
+        </nav>
+
+
+        <div className='flex items-center gap-3' style={{lineHeight:'0px'}}>Does this product have variants?  
+
+        <label className="switch">
+ <input type="checkbox" id="togBtn" checked={isChecked}
+          onChange={handleCheckboxChange} />
+ <div className="slider round"> 
+  <span className="on">ON</span>
+  <span className="off">OFF</span> 
+ </div>
+</label>
+
+          {/* <div className="relative inline-block w-11 h-5">
+          <input checked={isChecked}
+          onChange={handleCheckboxChange}
+            id="switch-component-greens" type="checkbox" className="peer appearance-none w-11 h-5 bg-red-500 rounded-full checked:bg-green-600 cursor-pointer transition-colors duration-300" />
           <label htmlFor="switch-component-greens" className="absolute top-0 left-0 w-5 h-5 bg-white rounded-full border border-slate-300 shadow-sm transition-transform duration-300 peer-checked:translate-x-6 peer-checked:border-green-600 cursor-pointer">
           </label>
-        </div> 
+        </div>  */}
           </div>
+      </div>
 
-        </div>
-       
-        
-         
-  
-        <div className="flex w-full gap-4">
+      <div className="mt-3">
+        <div
+          id="tabs-with-underline-1"
+          role="tabpanel"
+          aria-labelledby="tabs-with-underline-item-1"
+          className={activeTab === 1 ? '' : 'hidden'}
+        >
+            <div className="flex w-full gap-4">
           <div className=" w-full pb-8 my-5 border-b border-dashed border-border-base sm:my-8"> 
             <div className="mb-2">
           <Input
@@ -275,16 +358,17 @@ const AddProduct = ({ initialValues }: { initialValues?: Shop }) => {
             />  
             </div>
               </div>
-              <div className="mb-2">
+              <div className="mb-4">
               <Input
               label='Product SKU'
               {...register('name')}
-              variant="outline" 
+              variant="outline"
+              className="mb-0"
               placeholder='Product SKU'
             />  
               </div>
 
-              <div className="mb-2 flex justify-between gap-4"> 
+              <div className="mb-4 flex justify-between gap-4"> 
                 <div className='w-full'>
                 <label className="flex text-body-dark font-semibold text-sm leading-none mb-3">
                 Color
@@ -292,7 +376,8 @@ const AddProduct = ({ initialValues }: { initialValues?: Shop }) => {
                 <div className="">
                 <select className="px-4 flex items-center w-full rounded appearance-none transition duration-300 ease-in-out text-heading text-sm focus:outline-none focus:ring-0 border border-border-base focus:border-accent h-12">
                     <option value=" " selected>Select Color</option>
-                    <option value="female">{t('Female')}</option>
+                    <option value="Orange">Orange Navy</option>
+                    <option value="Yellow">Yellow Navy</option>
                   </select>
                 </div>
                 </div>
@@ -305,7 +390,15 @@ const AddProduct = ({ initialValues }: { initialValues?: Shop }) => {
                 <div className="">
                 <select className="px-4 flex items-center w-full rounded appearance-none transition duration-300 ease-in-out text-heading text-sm focus:outline-none focus:ring-0 border border-border-base focus:border-accent h-12">
                     <option value=" " selected>Select Size</option>
-                    <option value="female">{t('Female')}</option>
+                    <option value="female">Size Small   $50.00</option>
+                    <option value="female">Size Medium   $52.00</option>
+                    <option value="female">Size Large   $54.00</option>
+                    <option value="female">Size XL   $57.00</option>
+                    <option value="female">Size 2XL   $60.00</option>
+                    <option value="female">Size 3XL   $62.00</option>
+                    <option value="female">Size 4XL   $64.00</option>
+                    <option value="female">Size 5XL   $67.00</option>
+                    <option value="female">Size 7XL   $70.00</option>
                   </select>
                 </div>
                 </div>
@@ -317,84 +410,227 @@ const AddProduct = ({ initialValues }: { initialValues?: Shop }) => {
                 <div className="">
                   <select className="px-4 flex items-center w-full rounded appearance-none transition duration-300 ease-in-out text-heading text-sm focus:outline-none focus:ring-0 border border-border-base focus:border-accent h-12">
                     <option value=" " selected>Select Gender</option>
-                    <option value="female">{t('Female')}</option>
+                    <option value="Womwn">Women</option>
+                    <option value="Men">Men</option>
+                    <option value="Both">Both</option>
                   </select>
                 </div>
                 </div> 
               </div>
 
-              <div className="mb-5">
-                <label className="block text-sm font-medium text-gray-700">
-                  {t('Gender')}
+              <div className="mb-4">
+                <label className="flex text-body-dark font-semibold text-sm leading-none mb-3">
+                Select Brand
                 </label>
                 <div className="">
                   <select
                     {...register('gender')}
-                    className="border rounded px-2 w-full"
+                    className="px-4 flex items-center w-full rounded appearance-none transition duration-300 ease-in-out text-heading text-sm focus:outline-none focus:ring-0 border border-border-base focus:border-accent h-12"
                   >
-                    <option value="male">{t('Male')}</option>
+                    <option value="male">Select Brand</option>
                     <option value="female">{t('Female')}</option>
                   </select>
                 </div>
               </div>
 
-              <PhoneNumberInput
-                label={t('Contact No')}
-                // required
-                {...register('contact_no')}
-                control={control}
-                // error={t(errors.primary_contact_detail?.contact_no?.message!)}
-              />
+              <div className="mb-4">
+                <label className="flex text-body-dark font-semibold text-sm leading-none mb-3">
+                Category
+                </label>
+                <div className="">
+                  <select
+                    {...register('gender')}
+                    className="px-4 flex items-center w-full rounded appearance-none transition duration-300 ease-in-out text-heading text-sm focus:outline-none focus:ring-0 border border-border-base focus:border-accent h-12"
+                  >
+                    <option value=" " selected>Select Category</option>
+                    <option value="female">NSW Rail Clothing</option>
+                  </select>
+                </div>
+              </div>
+              <div className="mb-4">
+                <label className="flex text-body-dark font-semibold text-sm leading-none mb-3">
+                Sub Category
+                </label>
+                <div className="">
+                  <select
+                    {...register('gender')}
+                    className="px-4 flex items-center w-full rounded appearance-none transition duration-300 ease-in-out text-heading text-sm focus:outline-none focus:ring-0 border border-border-base focus:border-accent h-12"
+                  >
+                    <option value=" " selected>Select Sub Category</option>
+                    <option value="female">NSW Rail Shirts</option>
+                  </select>
+                </div>
+              </div>
+              <div className="mb-4">
+              <Input
+              label='Product Price'
+              {...register('name')}
+              variant="outline"
+              className="mb-0"
+              placeholder='$0'
+            />  
+              </div>
+              <div className="mb-4">
+              <Input
+              label='Sale Price'
+              {...register('name')}
+              variant="outline"
+              className="mb-0"
+              placeholder='$0'
+            />  
+              </div>
 
+              <div className="mb-4">
               <Input
-                label={t('Password')}
-                type="password"
-                {...register('password', {
-                  minLength: {
-                    value: 8,
-                    message: t('Password must be at least 8 characters'),
-                  },
-                })}
-                variant="outline"
-                className="mb-5"
-              />
-              <Input
-                type="date"
-                label={t('Job Title')}
-                {...register('joining_date')}
-                variant="outline"
-                className="mb-5"
-              />
-              <Input
-                label={t('Job Title')}
-                {...register('job_title')}
-                variant="outline"
-                className="mb-5"
-              />
+              label='Product Quantity'
+              {...register('name')}
+              variant="outline"
+              className="mb-0"
+              placeholder='0'
+            />  
+              </div>
 
+              <div className="mb-4">
               <Input
-                label={t('Tag')}
-                {...register('tag')}
-                variant="outline"
-                className="mb-5"
-              /> 
+              label='Tag'
+              {...register('name')}
+              variant="outline"
+              className="mb-0"
+              placeholder='Women Uniform'
+            />  
+              </div>
+ 
           </div>
         </div>
+        </div>
 
-        <StickyFooterPanel className="z-0">
-          <div className="mb-5 text-end">
-            <Button
-              type="submit"
-              loading={creating || updating}
-              disabled={creating || updating}
-              // onClick={onSubmit}
-            >
-              {initialValues
-                ? t('form:button-label-update')
-                : t('form:button-label-save')}
-            </Button>
-          </div>
-        </StickyFooterPanel>
+
+
+
+        <div
+          id="tabs-with-underline-2"
+          role="tabpanel"
+          aria-labelledby="tabs-with-underline-item-2"
+          className={activeTab === 2 ? '' : 'hidden'}
+        >
+<div className="grid grid-cols-2 gap-3 mb-4">
+
+         <Multiselect
+         placeholder='Select...'
+  displayValue="key"
+  onKeyPressFn={function noRefCheck(){}}
+  onRemove={function noRefCheck(){}}
+  onSearch={function noRefCheck(){}}
+  onSelect={function noRefCheck(){}}
+  options={[
+    {
+      cat: 'Group 1',
+      key: 'Color'
+    },
+    {
+      cat: 'Group 2',
+      key: 'Size'
+    },
+    {
+      cat: 'Group 3',
+      key: 'Material'
+    }
+  ]}
+  showCheckbox
+/>
+
+
+
+<Multiselect
+         placeholder='Select Color'
+  displayValue="key"
+  onKeyPressFn={function noRefCheck(){}}
+  onRemove={function noRefCheck(){}}
+  onSearch={function noRefCheck(){}}
+  onSelect={function noRefCheck(){}}
+  options={[
+    {
+      cat: 'Red 1',
+      key: 'Red'
+    },
+    {
+      cat: 'Green 2',
+      key: 'Green'
+    }
+  ]}
+  showCheckbox
+/>
+
+<Multiselect
+         placeholder='Select Size'
+  displayValue="key"
+  onKeyPressFn={function noRefCheck(){}}
+  onRemove={function noRefCheck(){}}
+  onSearch={function noRefCheck(){}}
+  onSelect={function noRefCheck(){}}
+  options={[
+    {
+      cat: 'Small 1',
+      key: 'Small'
+    },
+    {
+      cat: 'Medium 2',
+      key: 'Medium'
+    },
+    {
+      cat: 'Large 3',
+      key: 'Large'
+    } 
+  ]}
+  showCheckbox
+/>
+
+<Multiselect
+         placeholder='Select Material'
+  displayValue="key"
+  onKeyPressFn={function noRefCheck(){}}
+  onRemove={function noRefCheck(){}}
+  onSearch={function noRefCheck(){}}
+  onSelect={function noRefCheck(){}}
+  options={[ 
+    {
+      cat: 'ABC',
+      key: 'ABC'
+    }
+  ]}
+  showCheckbox
+/>
+        </div> 
+
+        <div className="text-right mb-5">
+          <Button className='mr-3 bg-black border border-black-600 text-white hover:bg-transprint-700  hover:bg-white hover:text-black  items-center pl-8 pr-8'>Generate Variants</Button>
+          <Button className='bg-transprent border border-red-600 text-red-600 hover:bg-transprint-700  hover:bg-white hover:text-black  items-center pl-8 pr-8'>Clear Variants</Button>
+        </div>
+
+        <div className='mt-5'>
+          <VariantTable 
+        shops={shops}
+        paginatorInfo={paginatorInfo}
+        onPagination={handlePagination}
+        onOrder={setOrder}
+        onSort={setColumn}
+        // @ts-ignore
+         />
+        </div>
+
+        </div> 
+      </div>  
+        </div>
+       
+        
+         
+  
+     
+<div className="grid grid-cols-2 gap-3 justify-between">
+<Button className='bg-transprent border border-red-600 text-red-600 hover:bg-transprint-700  hover:bg-white hover:text-black flex gap-2 items-center pl-8 pr-8'>Cancel</Button>
+<Button className='bg-black border border-black-600 text-white hover:bg-transprint-700  hover:bg-white hover:text-black flex gap-2 items-center pl-8 pr-8'>Add product</Button>
+</div>
+     
       </form>
     </>
   );
