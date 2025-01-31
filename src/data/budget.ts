abd4cec5d82db3a0267425bd4ff46a756edadf9f
techbase2brand <techbase2brand@gmail.com>
2025-01-31 20:27:14 +0530

    budget done

---
+++ src/data/budget.ts
@@ -0,0 +1,223 @@
+import { Config } from '@/config';
+import { Routes } from '@/config/routes';
+import { API_ENDPOINTS } from '@/data/client/api-endpoints';
+import { Shop, ShopPaginator, ShopQueryOptions } from '@/types';
+import { adminOnly, getAuthCredentials, hasAccess } from '@/utils/auth-utils';
+import { mapPaginatorData } from '@/utils/data-mappers';
+import { useTranslation } from 'next-i18next';
+import Router, { useRouter } from 'next/router';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { toast } from 'react-toastify';
+import { shopClient } from './client/shop';
+import { useState } from 'react';
+import { BudgetClient } from './client/budget';
+
+export const useApproveShopMutation = () => {
+  const { t } = useTranslation();
+  const queryClient = useQueryClient();
+  return useMutation(shopClient.approve, {
+    onSuccess: () => {
+      toast.success(t('common:successfully-updated'));
+    },
+    // Always refetch after error or success:
+    onSettled: () => {
+      queryClient.invalidateQueries(API_ENDPOINTS.SHOPS);
+    },
+  });
+};
+
+export const useDisApproveShopMutation = () => {
+  const { t } = useTranslation();
+  const queryClient = useQueryClient();
+  return useMutation(shopClient.disapprove, {
+    onSuccess: () => {
+      toast.success(t('common:successfully-updated'));
+    },
+    // Always refetch after error or success:
+    onSettled: () => {
+      queryClient.invalidateQueries(API_ENDPOINTS.SHOPS);
+    },
+  });
+};
+
+export const useDeleeteAllShopMutation = () => {
+  const queryClient = useQueryClient();
+  const router = useRouter();
+
+  return useMutation(shopClient.deleteAll, {
+    onSuccess: () => {
+      const { permissions } = getAuthCredentials();
+      // if (hasAccess(adminOnly, permissions)) {
+       router.push(`/company`);
+      // }
+      toast.success('Company Deleted Successfully!');
+      // router.push(Routes.dashboard);
+    },
+    // Always refetch after error or success:
+    onSettled: () => {
+      queryClient.invalidateQueries('company/deleteAll');
+    },
+  });
+};
+export const useCreateBudgetMutation = () => {
+  const queryClient = useQueryClient();
+  const router = useRouter();
+
+  return useMutation(BudgetClient.create, {
+    onSuccess: () => {
+      const { permissions } = getAuthCredentials();
+      if (hasAccess(adminOnly, permissions)) {
+        return router.push(`/budget`);
+      }
+      router.push(Routes.dashboard);
+    },
+    onError: (error) => {
+      console.error("Error creating shop:", error);
+
+    },
+    // Always refetch after error or success:
+    onSettled: () => {
+      queryClient.invalidateQueries(API_ENDPOINTS.COMPANY);
+    },
+  });
+};
+// export const useRegisterShopMutation = () => {
+//   const [isModalVisible, setModalVisible] = useState(false);
+//   const queryClient = useQueryClient();
+//   const router = useRouter();
+
+//   return useMutation(shopClient.create, {
+//     onSuccess: () => {
+//       // if (hasAccess(adminOnly, permissions)) {
+//       // return router.push(`/company`);
+//       // }
+//       // router.push(`/thanks`);
+//       setModalVisible(true)
+//       toast.success('Company Register successfully');
+//       // router.push(Routes.dashboard);
+//     },
+//     // Always refetch after error or success:
+//     onSettled: () => {
+//       queryClient.invalidateQueries(API_ENDPOINTS.COMPANY);
+//     },
+    
+//   });
+  
+// };
+export const useRegisterShopMutation = () => {
+  const [isModalVisible, setModalVisible] = useState(false); // State to control modal visibility
+  const queryClient = useQueryClient();
+  const router = useRouter();
+
+  const { mutate: registerUser } = useMutation(shopClient.create, {
+    onSuccess: () => {
+      console.log("Mutation success, opening modal...");
+      setModalVisible(true); // Open the modal on success
+      toast.success('Company registered successfully');
+      // Optionally navigate to a "thank you" page after registration
+      // router.push('/thanks');
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(API_ENDPOINTS.COMPANY); // Refetch queries
+    },
+  });
+
+  return {
+    registerUser,
+    isModalVisible,    // Ensure we are returning this state
+    setModalVisible,   // This allows you to modify modal visibility from other components
+  };
+};
+
+export const useUpdateShopMutation = () => {
+  const { t } = useTranslation();
+  const router = useRouter();
+  const queryClient = useQueryClient();
+  return useMutation(shopClient.update, {
+    onSuccess: async (data) => {
+      await router.push(`/company`, undefined, {
+        locale: Config.defaultLanguage,
+      });
+      toast.success(t('common:successfully-updated'));
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(API_ENDPOINTS.SHOPS);
+    },
+  });
+};
+export const useDeleteShopMutation = () => {
+  const { t } = useTranslation();
+  const router = useRouter();
+  const queryClient = useQueryClient();
+  return useMutation(shopClient.delete, {
+    onSuccess: async (data) => {
+      // await router.push(`/${data?.slug}/edit`, undefined, {
+      //   locale: Config.defaultLanguage,
+      // });
+      toast.success(t('Company Deleted Successfully!'));
+      
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(API_ENDPOINTS.SHOPS);
+    },
+  });
+};
+export const useTransferShopOwnershipMutation = () => {
+  const { t } = useTranslation();
+  const queryClient = useQueryClient();
+  return useMutation(shopClient.transferShopOwnership, {
+    onSuccess: (shop: Shop) => {
+      toast.success(
+        `${t('common:successfully-transferred')}${shop.owner?.name}`,
+      );
+    },
+    // Always refetch after error or success:
+    onSettled: () => {
+      queryClient.invalidateQueries(API_ENDPOINTS.SHOPS);
+    },
+  });
+};
+
+export const useShopQuery = ({ slug }: { slug: string }, options?: any) => {
+  return useQuery<Shop, Error>(
+    [API_ENDPOINTS.COMPANY, { slug }],
+    () => shopClient.get({ slug }),
+    options,
+  );
+};
+
+export const useShopsQuery = (options: Partial<ShopQueryOptions>) => {
+  const { data, error, isLoading } = useQuery<ShopPaginator, Error>(
+    [API_ENDPOINTS.SHOPS, options],
+    ({ queryKey, pageParam }) =>
+      shopClient.paginated(Object.assign({}, queryKey[1], pageParam)),
+    {
+      keepPreviousData: true,
+    },
+  );
+
+  return {
+    shops: data?.data ?? [],
+    paginatorInfo: mapPaginatorData(data),
+    error,
+    loading: isLoading,
+  };
+};
+
+export const useInActiveShopsQuery = (options: Partial<ShopQueryOptions>) => {
+  const { data, error, isLoading } = useQuery<ShopPaginator, Error>(
+    [API_ENDPOINTS.NEW_OR_INACTIVE_SHOPS, options],
+    ({ queryKey, pageParam }) =>
+      shopClient.newOrInActiveShops(Object.assign({}, queryKey[1], pageParam)),
+    {
+      keepPreviousData: true,
+    },
+  );
+
+  return {
+    shops: data?.data ?? [],
+    paginatorInfo: mapPaginatorData(data),
+    error,
+    loading: isLoading,
+  };
+};
--- src/pages/budget/index.tsx
+++ src/pages/budget/index.tsx
@@ -16,7 +16,8 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import Multiselect from 'multiselect-react-dropdown';
-
+import { useEmployeeGroupsQuery } from '@/data/employee-group';
+import { useCreateBudgetMutation } from '@/data/budget';
 
 export default function Budget() {
   const { t } = useTranslation();
@@ -26,18 +27,20 @@ export default function Budget() {
   const [searchTerm, setSearchTerm] = useState('');
   const [page, setPage] = useState(1);
   const [showPopup, setShowPopup] = useState(false);
- 
+  // State for form inputs
+  const [selectedGroups, setSelectedGroups] = useState<any[]>([]);
+  const [budget, setBudget] = useState<number | null>(null);
+  const [selectedDate, setSelectedDate] = useState<string | null>(null);
+
   const handlePopupToggle = () => {
     setShowPopup(!showPopup);
   };
 
- 
-
-  const handleSubmit = () => {
-    console.log('Uniform name:');
-    // Handle form submission here
-    setShowPopup(false); // Close the popup after submitting
-  };
+  // const handleSubmit = () => {
+  //   console.log('Uniform name:');
+  //   // Handle form submission here
+  //   setShowPopup(false); // Close the popup after submitting
+  // };
 
   const { coupons, loading, paginatorInfo, error } = useCouponsQuery({
     language: locale,
@@ -47,6 +50,48 @@ export default function Budget() {
     orderBy,
     sortedBy,
   });
+  const { mutate: createBudget, isLoading: creating } = useCreateBudgetMutation();
+
+  const { groups } = useEmployeeGroupsQuery({
+    // language: locale,
+    limit: 100,
+    page,
+    orderBy,
+    sortedBy,
+    name: searchTerm,
+  });
+
+
+  const handleSubmit = () => {
+    console.log('Selected Employees:', selectedGroups);
+    console.log('Budget:', budget);
+    console.log('Date:', selectedDate);
+    const payload = {
+      //@ts-ignore
+      budget: parseFloat(budget), // Ensure budget is a number
+      date: selectedDate,
+      groups: selectedGroups.map((group: any) => group.id), // Include only the group IDs
+    };
+    createBudget(
+      //@ts-ignore
+      payload,
+    );
+    // You can handle form submission here, e.g., send the data to an API
+    alert('Form submitted!');
+    setSelectedGroups([]);
+      //@ts-ignore
+    setBudget('');
+    setSelectedDate('');
+  };
+
+
+  function onSubmit() {
+    
+   
+    
+    }
+  
+  console.log('groups', groups);
 
   if (loading) return <Loader text={t('common:text-loading')} />;
   if (error) return <ErrorMessage message={error.message} />;
@@ -62,138 +107,93 @@ export default function Budget() {
 
   return (
     <>
-      <Card className="mb-8">
+      {/* <Card className="mb-8">
         <div className="flex flex-col items-center mb-4 md:flex-row">
-        <div className="mb-4 md:mb-0 md:w-1/3">
-          <PageHeading title='Budget' />
-        </div>
-
-        <div className="flex gap-4 flex-col items-center w-full space-y-4 ms-auto md:w-3/4 md:flex-row md:space-y-0 xl:w-1/1">
-       
-          <Search
-            onSearch={handleSearch}
-            placeholderText='Search...'
-          />  
-
-          <input type="text" className='ps-4 pe-4 h-12 flex items-center w-full xl:w-1/2 rounded-md appearance-none transition duration-300 ease-in-out text-heading text-sm focus:outline-none focus:ring-0 border border-border-base focus:border-accent bg-gray-100' placeholder='Spand Balance' disabled readOnly />
-          {/* <Button className='bg-black text-sm  text-white hover:bg-black-500'>
-            Filter</Button>
-            <Button className='bg-transprint text-sm  border border-black-500 text-black hover:bg-white-600'>
-            <svg className="mr-1" fill="#000" width="20px" height="20px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 16c1.671 0 3-1.331 3-3s-1.329-3-3-3-3 1.331-3 3 1.329 3 3 3z"></path><path d="M20.817 11.186a8.94 8.94 0 0 0-1.355-3.219 9.053 9.053 0 0 0-2.43-2.43 8.95 8.95 0 0 0-3.219-1.355 9.028 9.028 0 0 0-1.838-.18V2L8 5l3.975 3V6.002c.484-.002.968.044 1.435.14a6.961 6.961 0 0 1 2.502 1.053 7.005 7.005 0 0 1 1.892 1.892A6.967 6.967 0 0 1 19 13a7.032 7.032 0 0 1-.55 2.725 7.11 7.11 0 0 1-.644 1.188 7.2 7.2 0 0 1-.858 1.039 7.028 7.028 0 0 1-3.536 1.907 7.13 7.13 0 0 1-2.822 0 6.961 6.961 0 0 1-2.503-1.054 7.002 7.002 0 0 1-1.89-1.89A6.996 6.996 0 0 1 5 13H3a9.02 9.02 0 0 0 1.539 5.034 9.096 9.096 0 0 0 2.428 2.428A8.95 8.95 0 0 0 12 22a9.09 9.09 0 0 0 1.814-.183 9.014 9.014 0 0 0 3.218-1.355 8.886 8.886 0 0 0 1.331-1.099 9.228 9.228 0 0 0 1.1-1.332A8.952 8.952 0 0 0 21 13a9.09 9.09 0 0 0-.183-1.814z"></path></svg>
-            Reset</Button> */}
-        </div>
-        </div>
-         
-
-        <div className="flex gap-4 border border-black-500 p-4 items-center justify-between w-full rounded">
-        <div className='flex gap-4 w-1/1'>
-                 <Multiselect
-                 placeholder='Select Employee'
-          displayValue="key"
-          onKeyPressFn={function noRefCheck(){}}
-          onRemove={function noRefCheck(){}}
-          onSearch={function noRefCheck(){}}
-          onSelect={function noRefCheck(){}}
-          options={[
-            {
-              cat: 'Group 1',
-              key: 'John'
-            },
-            {
-              cat: 'Group 2',
-              key: 'Lily'
-            },
-            {
-              cat: 'Group 3',
-              key: 'Isla'
-            }
-          ]}
-          showCheckbox
-        /> 
-       <input type="number" className='ps-4 pe-4 h-12 flex items-center w-full xl:w-1/1 rounded-md appearance-none transition duration-300 ease-in-out text-heading text-sm focus:outline-none focus:ring-0 border border-border-base focus:border-accent' placeholder='$' />
-       <select id="day" name="day" className='ps-4 pe-4 h-12 flex items-center w-full xl:w-1/1 rounded-md appearance-none transition duration-300 ease-in-out text-heading text-sm focus:outline-none focus:ring-0 border border-border-base focus:border-accent'>
-        <option value="" selected>Day</option>
-        <option value="monday">Monday</option>
-        <option value="tuesday">Tuesday</option>
-        <option value="wednesday">Wednesday</option>
-        <option value="thursday">Thursday</option>
-        <option value="friday">Friday</option>
-        <option value="saturday">Saturday</option>
-        <option value="sunday">Sunday</option>
-    </select>
-    <select id="week" name="week" className='ps-4 pe-4 h-12 flex items-center w-full xl:w-1/1 rounded-md appearance-none transition duration-300 ease-in-out text-heading text-sm focus:outline-none focus:ring-0 border border-border-base focus:border-accent'>
-        <option value="" selected>Week</option>
-        <option value="week1">Week 1</option>
-        <option value="week2">Week 2</option>
-        <option value="week3">Week 3</option>
-        <option value="week4">Week 4</option>
-    </select>
-    <select id="month" name="month" className='ps-4 pe-4 h-12 flex items-center w-full xl:w-1/1 rounded-md appearance-none transition duration-300 ease-in-out text-heading text-sm focus:outline-none focus:ring-0 border border-border-base focus:border-accent'>
-        <option value="" selected>Month</option>
-        <option value="january">January</option>
-        <option value="february">February</option>
-        <option value="march">March</option>
-        <option value="april">April</option>
-        <option value="may">May</option>
-        <option value="june">June</option>
-        <option value="july">July</option>
-        <option value="august">August</option>
-        <option value="september">September</option>
-        <option value="october">October</option>
-        <option value="november">November</option>
-        <option value="december">December</option>
-    </select>
-      
-       </div>
-          <div>
-       <Button onClick={handlePopupToggle} className='bg-black text-sm text-white hover:bg-black-500'>
-         Submit</Button>
-         </div>
-     </div>
-      </Card>
-      <BudgetList
-        coupons={coupons}
-        paginatorInfo={paginatorInfo}
-        onPagination={handlePagination}
-        onOrder={setOrder}
-        onSort={setColumn}
-      />
-
-{showPopup && (
-        <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center">
-          <div className="bg-white p-6 rounded-lg w-1/3 text-center">
-
-            <div className="flex justify-center relative">
-            <svg width="65" height="65" viewBox="0 0 65 65" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M32.5 0C14.5631 0 0 14.5631 0 32.5C0 50.4369 14.5631 65 32.5 65C50.4369 65 65 50.4369 65 32.5C65 14.5631 50.4369 0 32.5 0ZM32.5 3.09524C48.7283 3.09524 61.9048 16.2717 61.9048 32.5C61.9048 48.7283 48.7283 61.9048 32.5 61.9048C16.2717 61.9048 3.09524 48.7283 3.09524 32.5C3.09524 16.2717 16.2717 3.09524 32.5 3.09524Z" fill="#E13232"/>
-            <path d="M32.5015 51.0713C34.2109 51.0713 35.5967 49.6856 35.5967 47.9761C35.5967 46.2666 34.2109 44.8809 32.5015 44.8809C30.792 44.8809 29.4062 46.2666 29.4062 47.9761C29.4062 49.6856 30.792 51.0713 32.5015 51.0713Z" fill="#E13232"/>
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M30.9531 17.0242V38.6908C30.9531 39.5451 31.6465 40.2385 32.5007 40.2385C33.355 40.2385 34.0484 39.5451 34.0484 38.6908V17.0242C34.0484 16.1699 33.355 15.4766 32.5007 15.4766C31.6465 15.4766 30.9531 16.1699 30.9531 17.0242Z" fill="#E13232"/>
-            </svg>
-
-            <a   onClick={handlePopupToggle} className='cursor-pointer' style={{position:'absolute', top:'-10px', right:'0px'}}>X</a>
-            </div>
-              <h2 className='text-center text-xl font-bold mb-4 mt-4'>Are you Sure?</h2>
-            <label htmlFor="" className='flex justify-center text-body-dark font-semibold text-xl leading-none mb-4'>Do you really want to add $500.</label>
-            
-            <div className="flex gap-5 mt-5 justify-center">
-            <Button
-                onClick={handlePopupToggle}
-                className="bg-transprint border border-red-500 text-red-500 px-4 py-2 rounded-md hover:bg-white-600"
-              >
-                No
-              </Button>
-              <Button
-                onClick={handleSubmit}
-                className="bg-black text-white px-4 py-2 rounded-md hover:bg-gray-800"
-              >
-                Yes
-              </Button>
-           
-            </div>
+          <div className="mb-4 md:mb-0 md:w-1/3">
+            <PageHeading title="Budget" />
           </div>
         </div>
-      )}
 
+        <div className="flex gap-4 border border-black-500 p-4 items-center justify-between w-full rounded">
+          <div className="flex gap-4 w-1/1">
+            <Multiselect
+              placeholder="Select Employee"
+              displayValue="name"
+              onKeyPressFn={function noRefCheck() {}}
+              onRemove={function noRefCheck() {}}
+              onSearch={function noRefCheck() {}}
+              onSelect={function noRefCheck() {}}
+              options={groups}
+              showCheckbox
+            />
+            <input
+              type="number"
+              className="ps-4 pe-4 h-12 flex items-center w-full xl:w-1/1 rounded-md appearance-none transition duration-300 ease-in-out text-heading text-sm focus:outline-none focus:ring-0 border border-border-base focus:border-accent"
+              placeholder="$"
+            />
+            <input
+              type="date"
+              className="ps-4 pe-4 h-12 flex items-center w-full xl:w-1/1 rounded-md appearance-none transition duration-300 ease-in-out text-heading text-sm focus:outline-none focus:ring-0 border border-border-base focus:border-accent"
+              placeholder="$"
+            />
+          </div>
+          <div>
+            <Button
+              onClick={handlePopupToggle}
+              className="bg-black text-sm text-white hover:bg-black-500"
+            >
+              Submit
+            </Button>
+          </div>
+        </div>
+      </Card> */}
+    {/* </> */}
+    <Card className="mb-8">
+        <div className="flex flex-col items-center mb-4 md:flex-row">
+          <div className="mb-4 md:mb-0 md:w-1/3">
+            <PageHeading title="Budget" />
+          </div>
+        </div>
+
+        <div className="flex gap-4 border border-black-500 p-4 items-center justify-between w-full rounded">
+          <div className="flex gap-4 w-full">
+            {/* Multiselect Input */}
+            <Multiselect
+              placeholder="Select Employee"
+              displayValue="name"
+              onSelect={(selectedList) => setSelectedGroups(selectedList)}
+              onRemove={(selectedList) => setSelectedGroups(selectedList)}
+              options={groups || []}
+              showCheckbox
+            />
+
+            {/* Number Input */}
+            <input
+              type="number"
+              value={budget ?? ''}
+              onChange={(e) => setBudget(Number(e.target.value))}
+              className="ps-4 pe-4 h-12 flex items-center w-full xl:w-1/1 rounded-md appearance-none transition duration-300 ease-in-out text-heading text-sm focus:outline-none focus:ring-0 border border-border-base focus:border-accent"
+              placeholder="Enter Budget ($)"
+            />
+
+            {/* Date Input */}
+            <input
+              type="date"
+              value={selectedDate ?? ''}
+              onChange={(e) => setSelectedDate(e.target.value)}
+              className="ps-4 pe-4 h-12 flex items-center w-full xl:w-1/1 rounded-md appearance-none transition duration-300 ease-in-out text-heading text-sm focus:outline-none focus:ring-0 border border-border-base focus:border-accent"
+            />
+          </div>
+          <div>
+            <Button
+              onClick={handleSubmit}
+              className="bg-black text-sm text-white hover:bg-black-500"
+            >
+              Submit
+            </Button>
+          </div>
+        </div>
+      </Card>
     </>
   );
 }
